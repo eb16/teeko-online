b3b94caaf15d8c9e8a91e5b88c47e61440be80dc
Emiliano Botti <emiliano.botti@fing.edu.uy>
2020-03-15 03:09:34 -0300

    Updates

---
+++ precache-manifest.e8fd6621ea9b1a8c368e8f2146de51d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030dcb2b4bab37a135dacf8b838d7220",
+    "url": "/teeko-online/index.html"
+  },
+  {
+    "revision": "45fb9fc2fd25fbb46440",
+    "url": "/teeko-online/static/css/main.9bca530d.chunk.css"
+  },
+  {
+    "revision": "bccf0e67d603a98a7ed1",
+    "url": "/teeko-online/static/js/2.0faee036.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/teeko-online/static/js/2.0faee036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45fb9fc2fd25fbb46440",
+    "url": "/teeko-online/static/js/main.0d14f0a4.chunk.js"
+  },
+  {
+    "revision": "c5c28d2fc0b02a8b37ca",
+    "url": "/teeko-online/static/js/runtime-main.572a3361.js"
+  }
+]);
